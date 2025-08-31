5f4f7292b09d3a3a059840a64fe333cd7bda35ab
khazaanacoupon-hub <khazaanacoupon@gmail.com>
2025-08-31 19:52:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -75,7 +75,7 @@ connectDB()
 
 // Middleware
 app.use(cors({
-  origin: "https://khazaana-8jzy.onrender.com", // your frontend URL
+  origin: "https://khazaana-fro.onrender.com", // your frontend URL
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
